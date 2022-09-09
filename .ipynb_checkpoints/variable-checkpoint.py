46f36f9092bdca434c187f29865a2168ec0a72b1
alelouis <me@alelouis.eu>
2022-09-09 17:09:01 +0200

    remove ipynb checkpoints

--- .ipynb_checkpoints/variable-checkpoint.py
+++
@@ -1,9 +0,0 @@
-class Variable:
-    def __init__(self, data=None, label=None):
-        self.data = data
-        self.label = label
-        self.grad = 0.0
-        self._backward = lambda _: None
-
-    def __repr__(self):
-        return f"Variable({self.label}, data = {self.data}, grad = {self.grad})"
